ce4f038eb7457d3ee41b93af1f6007480e58513c
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 12:33:13 -0700

    added input and reversing functions

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -10,3 +10,22 @@ a long string containing multiple words. Return to the user the same string,
 with the words in backward order.
 
 """
+
+def user_input():
+    string = input("\nPlease enter a sentence that you would like reversed:\n")
+    string = str(string)
+    return string
+
+
+def reverse_string(user_input):
+    ## Breaking the string into individual words using
+    ## a space as the delimiter
+    split_words = user_input.split(' ')
+    
+    reversed_string = split_words[::-1]
+
+    return reversed_string
+
+
+if __name__ == '__main__':
+    print(reverse_string(user_input()))
