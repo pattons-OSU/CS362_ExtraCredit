78606dc08e78bfd42a2f9ec629407ceda0a15c8c
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 14:45:12 -0700

    addint comments to test_reverse

--- test_reverse_sentence.py
+++ test_reverse_sentence.py
@@ -14,6 +14,9 @@ import unittest
 import pytest
 import reverse_sentence
 
+## Could not get the pytest fixtures working correctly.
+## as of this moment, you need to run pytest with the -s
+## flag in order to obtain user input.
 """
 @pytest.fixture
 def user_input():
@@ -25,14 +28,18 @@ def reversed_input():
     reversed_input = reverse_sentence.reverse_string(user_input)
     return reversed_input
 """
+
+## Setting global variables
 user_input = reverse_sentence.user_input()
 reversed_input = reverse_sentence.reverse_string(user_input)
 
 class testCase(unittest.TestCase):
-    
+    ## Testing to make sure that the user is passing through a
+    ## String that is capable of being reversed.
     def test_input_type(self):
         self.assertTrue(type(user_input), str)
-
+    ## Testing to make sure that the reversing function is actually
+    ## doing the correct function.
     def test_reversing(self):
         self.assertEqual(user_input.split(' ')[::-1], reversed_input)
 
