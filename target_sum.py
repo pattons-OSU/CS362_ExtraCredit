668eab4b25bb1c1451ca47981f907866feb4235b
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 14:50:00 -0700

    creating target_sum program structure

---
+++ target_sum.py
@@ -0,0 +1,12 @@
+#! python3
+
+"""
+Simeon Patton
+CS362 OSU Spring 2021
+Extra Credit 3 - Array and target sum
+
+--Write a program that takes an array of integers - array and an integer target_sum.
+Your program should return an array containing two numbers that add up to the target.
+"""
+
+import math
