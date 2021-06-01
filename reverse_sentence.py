73f722f8618331f7fd80d0cc8b01788a91ee76ab
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 12:11:04 -0700

    creating reverse_sentence program strcture

---
+++ reverse_sentence.py
@@ -0,0 +1,12 @@
+#! python3
+
+"""
+Simeon Patton
+CS362 OSU Spring 2021
+Extra Credit 1.1 - Reverse sentence program
+
+--Write a program that reverses a sentence. Input - ask the user for a sentence:
+a long string containing multiple words. Return to the user the same string,
+with the words in backward order.
+
+"""
