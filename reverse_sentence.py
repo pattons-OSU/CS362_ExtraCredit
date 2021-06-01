93df5af10fd9a422d40b3dbe32ef31e0ca17c73b
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 12:51:37 -0700

    fully running program with correct output

--- reverse_sentence.py
+++ reverse_sentence.py
@@ -12,7 +12,7 @@ with the words in backward order.
 """
 
 def user_input():
-    string = input("\nPlease enter a sentence that you would like reversed:\n")
+    string = input("\nPlease enter a sentence that you would like reversed:\n\n")
     string = str(string)
     return string
 
@@ -21,11 +21,22 @@ def reverse_string(user_input):
     ## Breaking the string into individual words using
     ## a space as the delimiter
     split_words = user_input.split(' ')
-    
+    ## Reversing the list of strings
     reversed_string = split_words[::-1]
-
     return reversed_string
+
+def merge_reversed(reversed_string):
+    word_list = reversed_string
+    deliminator = ' '
+    ## Using .join method to concantinate into a new string
+    merged_string = deliminator.join(word_list)
+    return merged_string
 
 
 if __name__ == '__main__':
-    print(reverse_string(user_input()))
+    string_input = user_input()
+    separated_reversed_list = reverse_string(string_input)
+    concantinated_reversed_list = merge_reversed(separated_reversed_list)
+    print("\nYour reversed sentence is:\n\n")
+    print(concantinated_reversed_list)
+    print("\n")
