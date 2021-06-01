8e3da3ed90f1203e6f5e4e70ebe9d95c5c3618e3
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 14:01:25 -0700

    working on getting pytest working w/ input

--- test_reverse_sentence.py
+++ test_reverse_sentence.py
@@ -9,3 +9,33 @@ Extra Credit 1.2 - Unittest and pytest the Reverse sentence program
 (do not run the tests with pytest alone - you should use pytest syntax as well.)
 
 """
+
+import unittest
+import pytest
+import reverse_sentence
+
+"""
+@pytest.fixture
+def user_input():
+    user_input = reverse_sentence.user_input()
+    return user_input
+
+@pytest.fixture
+def reversed_input():
+    reversed_input = reverse_sentence.reverse_string(user_input)
+    return reversed_input
+"""
+user_input = reverse_sentence.user_input()
+reversed_input = reverse_sentence.reverse_string(user_input)
+
+class testCase(unittest.TestCase):
+    
+    def test_input_type(self):
+        self.assertTrue(type(user_input), str)
+
+    def test_reversing(self):
+        self.assertEqual(user_input.split(' ')[::-1], reversed_input)
+
+
+if __name__ == '__main__':
+    unittest.main()
