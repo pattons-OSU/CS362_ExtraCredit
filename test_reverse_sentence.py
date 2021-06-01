639bb973fc314b005a46a8abdb81891c7d69533c
pattons-OSU <pattons@oregonstate.edu>
2021-06-01 12:54:15 -0700

    creating test_reverse_sentence prgm structure

---
+++ test_reverse_sentence.py
@@ -0,0 +1,11 @@
+#! python3
+
+"""
+Simeon Patton
+CS362 OSU Spring 2021
+Extra Credit 1.2 - Unittest and pytest the Reverse sentence program
+
+--Write tests for the reverse_sentence program using unittest and pytest. 
+(do not run the tests with pytest alone - you should use pytest syntax as well.)
+
+"""
