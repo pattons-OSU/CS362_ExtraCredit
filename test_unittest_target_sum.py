8ff9f1255b3f1a995118fb81ec3045f99009ef89
pattons-OSU <pattons@oregonstate.edu>
2021-06-03 12:16:33 -0700

    creating pytest and unittest skeletons

---
+++ test_unittest_target_sum.py
@@ -0,0 +1,14 @@
+#! python3
+
+"""
+Simeon Patton
+CS362 OSU Spring 2021
+Extra Credit 3.1 - Unittest the target sum program
+
+--Write tests for the target_sum program using unittest and pytest. 
+(do not run the tests with pytest alone - you should use pytest syntax as well.)
+
+"""
+
+import unittest
+import target_sum
