8fe343e37f3a5f9df1194bb6da59001cf3d580c3
pattons-OSU <pattons@oregonstate.edu>
2021-06-03 11:59:42 -0700

    changing names of test programs to match type

--- test_pytest_reverse_sentence.py
+++ test_pytest_reverse_sentence.py
@@ -10,7 +10,7 @@ Extra Credit 1.2.1 - Unittest the Reverse sentence program
 
 """
 
-import unittest
+import pytest
 import reverse_sentence
 
 ## Could not get the pytest fixtures working correctly.
@@ -32,16 +32,22 @@ def reversed_input():
 user_input = reverse_sentence.user_input()
 reversed_input = reverse_sentence.reverse_string(user_input)
 
-class testCase(unittest.TestCase):
+class testCase():
+    """
+    @pytest.fixture
+    def user_input():
+        user_input = reverse_sentence.user_input()
+        reversed_input = reverse_sentence.reverse_string(user_input)
+        return reversed_input, user_input
+    """
     ## Testing to make sure that the user is passing through a
     ## String that is capable of being reversed.
     def test_input_type(self):
-        self.assertTrue(type(user_input), str)
+        assert type(user_input) == str
+        ##self.assertTrue(type(user_input), str)
     ## Testing to make sure that the reversing function is actually
     ## doing the correct function.
     def test_reversing(self):
-        self.assertEqual(user_input.split(' ')[::-1], reversed_input)
+        ##self.assertEqual(user_input.split(' ')[::-1], reversed_input)
+        assert user_input.split(' ')[::-1] == reversed_input
 
-
-if __name__ == '__main__':
-    unittest.main()
