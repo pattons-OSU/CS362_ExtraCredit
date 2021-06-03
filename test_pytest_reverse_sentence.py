d630dd3f10a624d7f70898e3e820f3860094b45e
pattons-OSU <pattons@oregonstate.edu>
2021-06-03 12:13:24 -0700

    pytest documentation update

--- test_pytest_reverse_sentence.py
+++ test_pytest_reverse_sentence.py
@@ -3,7 +3,7 @@
 """
 Simeon Patton
 CS362 OSU Spring 2021
-Extra Credit 1.2.1 - Unittest the Reverse sentence program
+Extra Credit 1.2.2 - Pytest the Reverse sentence program
 
 --Write tests for the reverse_sentence program using unittest and pytest. 
 (do not run the tests with pytest alone - you should use pytest syntax as well.)
