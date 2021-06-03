e79d3334f876449e3ccf2ae155d57a5dc3520e1f
pattons-OSU <pattons@oregonstate.edu>
2021-06-03 15:29:46 -0700

    adding comments to finished pytest tests

--- test_pytest_target_sum.py
+++ test_pytest_target_sum.py
@@ -22,16 +22,19 @@ test_target_sum = target_sum.target_sum()
 test_array_list = target_sum.add_to_array(test_array_size)
 test_pair_value = target_sum.pair_values(test_array_list, test_array_size, test_target_sum)
 
-
+## Test to make sure that the input type is the correct data type of int
 def test_input_array_type():
     assert type(test_array_size) == int
 
+## Test to make sure that the input type is the correct data type of int
 def test_input_sum_type():
     assert type(test_target_sum) == int
 
+## Test to verify that the length of the list is what was intended by the inputs
 def test_add_to_array():
     assert len(test_array_list) == test_array_size
 
+## Test to verify that the ordered pairs equal the target sum
 def test_pair_vs_sum():
     assert test_pair_value[0][0] + test_pair_value[0][1] == test_target_sum
 
