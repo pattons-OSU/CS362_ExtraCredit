8a1306ca54ffa0b3eaf4a3c689d6be3857c065c9
pattons-OSU <pattons@oregonstate.edu>
2021-06-03 13:13:32 -0700

    adding tests to pytest prg

--- test_pytest_target_sum.py
+++ test_pytest_target_sum.py
@@ -12,3 +12,22 @@ Extra Credit 3.2 - Pytest the target sum program
 
 import pytest
 import target_sum
+
+## Setting global variables
+test_array_size = target_sum.array_size()
+target_sum_test = target_sum.target_sum()
+test_array_list = target_sum.add_to_array(test_array_size)
+
+
+def test_input_array_type():
+    assert type(test_array_size) == int
+
+def test_input_sum_type():
+    assert type(target_sum_test) == int
+
+def test_add_to_array():
+    assert len(test_array_list) == test_array_size
+
+
+    
+
